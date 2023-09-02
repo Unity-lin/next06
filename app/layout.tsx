78b51531c68a493aafb7c321b44524938c5d7643
ichaehun <ichaehunn@gmail.com>
2023-09-02 11:53:33 +0900

    text

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
+import Navbar from './components/Navbar'
+import MyProfiles from './components/MyProfiles'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -16,7 +18,11 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <Navbar/>
+        <MyProfiles/> 
+        {children}
+        </body>
     </html>
   )
 }
